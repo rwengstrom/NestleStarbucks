d559d868b57cc7ae01414380d09cf890daebf83f
rwengstrom <79719265+rwengstrom@users.noreply.github.com>
2021-04-20 12:28:21 -0700

    Initial Layout

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
+app = dash.Dash(__name__)
+
+# App Layout
+
+app.layout = html.Div([
+
+    html.H1("Nestle Starbucks Social Media Data", style={'text-align': 'center'}),
+
+    dcc.Dropdown(
+        options=[
+            {'label': 'Nestle', 'value': 'Nestle'},
+            {'label': 'Starbucks', 'value': 'Starbucks'},
+            {'label': 'Coffee', 'value': 'Coffee'}
+        ],
+        value='MTL'
+    )
+])
+
+# Run App
+if __name__ == '__main__':
+    app.run_server(debug = True)
