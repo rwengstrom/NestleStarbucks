b8e34f468fe564931237e70b851740d355ca366b
Wil Engstrom <wilson.engstrom@gmail.com>
2021-04-24 11:08:59 -0700

    Added Calendar and Input

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-import plotly.express as px
+from datetime import date
+from dash.dependencies import Input
 
 
-app = dash.Dash(__name__)
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # App Layout
 
@@ -18,10 +21,21 @@ dropdown = dcc.Dropdown(
         value='MTL'
     )
 
+
+daterange = dcc.DatePickerRange(
+        end_date=date(2021,4,22),
+        display_format='MM/DD/YY',
+        start_date_placeholder_text='MM/DD/YY'
+    )
+
 app.layout = html.Div([
 
     html.H1("Nestle Starbucks Social Media Data", style={'text-align': 'center'}),
-    dbc.Row([dbc.Col(dropdown, width=12)]), 
+    dbc.Row([dbc.Col(dropdown, width=12)]),
+    html.Br(),
+    dbc.Row([dbc.Col(daterange, width = 12)]),
+    html.Br(),
+    dcc.Input(id="input1", type="text", placeholder="Input Text Here"),
     html.Br(),
     html.Img(src='/assets/wordcloud/twitter cloud 1.png', style={'height':'20%', 'width':'20%'}),
     html.Br(),
@@ -29,6 +43,11 @@ app.layout = html.Div([
     
 ])
 
+@app.callback(
+    Input("input1", "value"),
+)
+def update_output(input1):
+    return u'Input 1 {}'.format(input1)
 
 # Run App
 if __name__ == '__main__':
