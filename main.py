df79f9b34b4eea084654c2d86541f6116c77f8c6
Wil Engstrom <wilson.engstrom@gmail.com>
2021-04-27 19:05:39 -0700

    Updated Functions

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 from datetime import date
 import makeclouds as wc
 import DataFetching as fetch
